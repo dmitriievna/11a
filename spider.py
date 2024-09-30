61ee21d77d754d666fe0d089b319fdbf75b12b93
dmitriievna <posazennikovaanna13@gmail.com>
2024-09-30 13:16:14 +0300

    Add files via upload

---
+++ spider.py
@@ -0,0 +1,20 @@
+
+print('hello world')
+
+a = 10  # a 
+if a > 10 :
+ pass 
+
+a = 10
+b = 5,0
+print (a + b) 
+print (a - b) 
+print (a / b) 
+print (a * b) 
+print (a ** b) 
+print (a // b) 
+
+
+user = input('enter your name:')
+print(f"hello{user}!")
+user2 = int(input('enter number:')) 
